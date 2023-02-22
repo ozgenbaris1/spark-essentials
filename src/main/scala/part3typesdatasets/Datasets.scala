9550c97c21bfbec23b8fd9d739bccd2e62d1a85a
Barış Özgen <barisozgen96@gmail.com>
2023-02-22 13:55:28 +0300

    Add datasets

---
+++ src/main/scala/part3typesdatasets/Datasets.scala
@@ -0,0 +1,65 @@
+package part3typesdatasets
+
+import org.apache.spark.sql.{SparkSession, Encoders, Dataset}
+import org.apache.spark.sql.functions._
+object Datasets extends App {
+  val spark = SparkSession
+    .builder()
+    .appName("Datasets")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val numbersDF = spark.read
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .csv("src/main/resources/data/numbers.csv")
+
+  // convert a DF to a Dataset
+  implicit val intEncoder = Encoders.scalaInt
+  val numbersDS: Dataset[Int] = numbersDF.as[Int]
+
+  // dataset of a complex type
+  // 1 - define your case class
+  case class Car(
+      Name: String,
+      Miles_per_Gallon: Option[Double], // nullable
+      Cylinders: Long,
+      Displacement: Double,
+      Horsepower: Option[Long],
+      Weight_in_lbs: Long,
+      Acceleration: Double,
+      Year: String,
+      Origin: String
+  )
+
+  // 2 - read the DF from the file
+  def readDF(filename: String) = spark.read
+    .option("inferSchema", "true")
+    .json(s"src/main/resources/data/$filename")
+
+  // 3 - define an encoder (by importing the implicits)
+  import spark.implicits._
+  val carsDF = readDF("cars.json")
+
+  // 4 - convert dataframe to dataset
+  val carsDS = carsDF.as[Car]
+
+  // DS collection functions
+  numbersDS.filter(_ < 100).show()
+
+  // map, flatMap, fold, reduce, for comprehension ...
+  val carNamesDS = carsDS.map(car => car.Name.toUpperCase())
+
+  /** Exercises
+    *   1. Count how many cars we have 2. Count how many POWERFUL cars we have
+    *      (HP > 140) 3. Average HP for the entire dataset
+    */
+
+  val carsCount = carsDS.count
+  println(carsCount)
+
+  println(carsDS.filter(_.Horsepower.getOrElse(0L) > 140).count)
+
+  println(carsDS.map(_.Horsepower.getOrElse(0L)).reduce(_ + _) / carsCount)
+
+}
